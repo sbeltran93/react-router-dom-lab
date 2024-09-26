a0672457a83d9f4be357772f545a785cfef08361
Sammy <samanthabeltran1993@gmail.com>
2024-09-26 17:00:55 -0500

    wip

--- src/components/MailboxDetails.jsx
+++ src/components/MailboxDetails.jsx
@@ -2,13 +2,13 @@
 import React from "react";
 import NavBar from "./NavBar";
 
-const MailboxDetails = () => {
-    return (
+const MailboxDetails = (props) => {
+    
     const { mailboxId } = useParams();
     const selectedBox = props.mailboxes.find(
     (mailbox) => mailbox._id === Number(mailboxId)
 );
-    )
+    
 }
 
 
